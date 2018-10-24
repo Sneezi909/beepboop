3f3c587dd068f957b4bbc00a171742e999486cfd
Vincent T <vincentt@berkeley.edu>
2018-10-24 00:21:04 -0700

    Did some more changes to program execution

--- fileIO.py
+++ fileIO.py
@@ -1,4 +1,7 @@
-def convert(file_name, array): 
+import * from ailyn.py
+
+
+def convert_to_file(file_name, array): 
     # Outputs the array into a file with the specified fileName for the bash script to read
     with open(file_name, "a+") as f:
         for note in array:
@@ -13,3 +16,14 @@ def note_to_string(note):
     freq, duration = note
     string = str(freq) + ","  + str(duration) + "\n"
     return string
+
+if __name__== "__main__":
+    # Run the array and collect them
+    if len(sys.argv) != 2: 
+        print("Incorrect Arguments, check to see if the midi file exists in the given directory.")
+        sys.exit()
+    low, mid, high = # Insert array conversion from midi
+    convert_to_file(low)
+    convert_to_file(mid)
+    convert_to_file(high)
+
