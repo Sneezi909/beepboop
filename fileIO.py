34c794dd943dec94380fd8f4634b5f89fe9b2592
Vincent T <vincentt@berkeley.edu>
2018-10-24 00:03:36 -0700

    Finished with the fileIO stuff

---
+++ fileIO.py
@@ -0,0 +1,15 @@
+def convert(file_name, array): 
+    # Outputs the array into a file with the specified fileName for the bash script to read
+    with open(file_name, "a+") as f:
+        for note in array:
+            string = note_to_string(note)
+            f.write(string)
+
+
+
+def note_to_string(note): 
+    # Helper function to convert.
+    # Takes a tuple and writes it into the correct form. See the test file
+    freq, duration = note
+    string = str(freq) + ","  + str(duration) + "\n"
+    return string
